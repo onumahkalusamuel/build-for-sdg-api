13226861c0ecb6053c7e0048e492b58f6f174fd2
Onumah Kalu Samuel <52772331+onumahkalusamuel@users.noreply.github.com>
2020-04-14 20:27:13 +0100

    Update logs.php

--- logs.php
+++ logs.php
@@ -1,11 +1,11 @@
 <?php
 $GLOBALS['starttime'] = microtime(true);
-header('Content-Type: text/plain');
+header('Content-Type: text/plain; charset=utf-8');
 http_response_code(200);
 $code = 200;
 $file = dirname(__FILE__). '/logs.txt';
 if(is_file($file)) {
-    echo file_get_contents($file);
+    echo trim(file_get_contents($file));
 } else {
     echo "No logs yet.";
 }
